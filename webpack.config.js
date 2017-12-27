0f1c378ab7b473dc1badf439785aa9e6fc62344e
domi <dominik.stoetter@yahoo.com>
2017-12-27 13:16:25 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: [
+    './src/'
+  ],
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'bundle.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body',
+    })
+  ]
+}
